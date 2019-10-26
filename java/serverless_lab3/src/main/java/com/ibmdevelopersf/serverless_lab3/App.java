166e9cad35f75a1a3f3c2722e6cf7595673edf94
Upkar Lidder <lidderupk@gmail.com>
2019-10-25 18:36:18 -0700

    initial commit

---
+++ java/serverless_lab3/src/main/java/com/ibmdevelopersf/serverless_lab3/App.java
@@ -0,0 +1,13 @@
+package com.ibmdevelopersf.serverless_lab3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
